17cdb2eb92306e0d20d7ebc6bcb4be8b4f894ec7
mrowl01 <christopherortega56@gmail.com>
2017-10-21 19:55:45 -0500

    First Change

---
+++ Assets/RandomScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		print ("First Change"); 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
